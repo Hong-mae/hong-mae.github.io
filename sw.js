8f944a95d12f9afb6b3f302ab01aec73ac02e8fd
Colin Park (Hanbin Park) <qkrgksqls9@gmail.com>
2021-09-15 01:48:52 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,29 +27,29 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1d74f1cccb83d070c798.js"
+    "url": "webpack-runtime-686f4fc2a519bff9cf29.js"
   },
   {
-    "url": "styles.4f3ccac8e8fe1e8cb92d.css"
+    "url": "styles.414e5d3aab42a27e148e.css"
   },
   {
     "url": "styles-407fe62976dc5310c43e.js"
   },
   {
-    "url": "framework-1e331b2e9d55ebef6dec.js"
+    "url": "framework-b2f816f437730bdc94bf.js"
   },
   {
     "url": "532a2f07-92db97c0addf07d5cb73.js"
   },
   {
-    "url": "dc6a8720040df98778fe970bf6c000a41750d3ae-facf7abfc3a171288a3b.js"
+    "url": "dc6a8720040df98778fe970bf6c000a41750d3ae-f4cc6a09c22b4ad5dfe4.js"
   },
   {
-    "url": "app-a212e4fb7eea4f092c14.js"
+    "url": "app-e3644d19451cb1aca493.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2626de48c2b09990f0e5a4a3056a75c8"
+    "revision": "9dd11dc860877c83f724a5418660c307"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-fd4fb51a6fac1c18bdde.js"
@@ -155,7 +155,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-a212e4fb7eea4f092c14.js`))) {
+  if (!resources || !(await caches.match(`/app-e3644d19451cb1aca493.js`))) {
     return await fetch(event.request)
   }
 
