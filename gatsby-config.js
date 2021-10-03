eb89015a1b79bd10873b9d8b2d33303944fc9b22
Colin Park (Hanbin Park) <qkrgksqls9@gmail.com>
2021-10-03 16:03:11 +0900

    연결리스트 포스팅

--- gatsby-config.js
+++ gatsby-config.js
@@ -207,7 +207,7 @@ module.exports = {
                 short_name: siteConfig.title,
                 start_url: '/',
                 background_color: '#FFF',
-                theme_color: '#F7A046',
+                theme_color: '#5D93FF',
                 display: 'standalone',
                 icon: 'static/logo512.png',
             },
